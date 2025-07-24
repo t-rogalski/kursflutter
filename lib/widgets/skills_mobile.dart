f851ed735df8a890ba5fa2fc0871b5f13b47c1a3
Staz06_mac <staz06_mac@Staz06-macs-MacBook-Pro.local>
2025-07-24 11:29:18 +0200

    Małe popoprawki wizualne

--- lib/widgets/skills_mobile.dart
+++ lib/widgets/skills_mobile.dart
@@ -7,46 +7,49 @@ class SkillsMobile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        for (int i = 0; i < platformItems.length; i++)
-          Container(
-            margin: EdgeInsets.only(bottom: 5),
-            width: double.maxFinite,
-            decoration: BoxDecoration(
-              color: CustomColor.bgLight2,
-              borderRadius: BorderRadius.circular(5),
-            ),
-            child: ListTile(
-              contentPadding: EdgeInsets.symmetric(
-                vertical: 10,
-                horizontal: 20,
+    return ConstrainedBox(
+      constraints: BoxConstraints(maxWidth: 450),
+      child: Column(
+        children: [
+          for (int i = 0; i < platformItems.length; i++)
+            Container(
+              margin: EdgeInsets.only(bottom: 5),
+              width: double.maxFinite,
+              decoration: BoxDecoration(
+                color: CustomColor.bgLight2,
+                borderRadius: BorderRadius.circular(5),
               ),
-              leading: Icon(platformItems[i]["img"]),
-              title: Text(platformItems[i]["title"]),
-            ),
-          ),
-        SizedBox(height: 50),
-
-        Wrap(
-          spacing: 10,
-          runSpacing: 10,
-          children: [
-            for (int i = 0; i < skillItems.length; i++)
-              Chip(
-                backgroundColor: CustomColor.bgLight2,
-                labelStyle: TextStyle(color: CustomColor.whitePrimary),
-                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                label: Text(skillItems[i]["title"]),
-                avatar: Icon(
-                  skillItems[i]["img"],
-                  color: CustomColor.whitePrimary,
-                  size: 20,
+              child: ListTile(
+                contentPadding: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 20,
                 ),
+                leading: Icon(platformItems[i]["img"]),
+                title: Text(platformItems[i]["title"]),
               ),
-          ],
-        ),
-      ],
+            ),
+          SizedBox(height: 50),
+
+          Wrap(
+            spacing: 10,
+            runSpacing: 10,
+            children: [
+              for (int i = 0; i < skillItems.length; i++)
+                Chip(
+                  backgroundColor: CustomColor.bgLight2,
+                  labelStyle: TextStyle(color: CustomColor.whitePrimary),
+                  padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                  label: Text(skillItems[i]["title"]),
+                  avatar: Icon(
+                    skillItems[i]["img"],
+                    color: CustomColor.whitePrimary,
+                    size: 20,
+                  ),
+                ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
